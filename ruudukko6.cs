e8c079d9c507e61ebadc8697f3bbb7828130f5ee
Mustafaa4 <Mustafa.Abdi@edu.bc.fi>
2026-09-23 04:55:16 +0300

    Update ruudukko6.cs

--- ruudukko6.cs
+++ ruudukko6.cs
@@ -1,41 +1,41 @@
-﻿using System;
-
-struct Koordinaatti
+﻿﻿namespace Tehtävä_6___Ruudukko
 {
-    public int X { get; private set; }
-    public int Y { get; private set; }
-
-    public Koordinaatti(int x, int y)
+    internal class Program
     {
-        X = x;
-        Y = y;
-    }
+        struct Koordinaatti
+        {
+            public int X { get; private set; }
+            public int Y { get; private set; }
 
-    
-    public bool OnVierekkaen(Koordinaatti toinen)
-    {
-       
-        return (Math.Abs(this.X - toinen.X) == 1 && this.Y == toinen.Y)
-            || (Math.Abs(this.Y - toinen.Y) == 1 && this.X == toinen.X);
-    }
+            public Koordinaatti(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
 
-    public override string ToString()
-    {
-        return $"({X},{Y})";
-    }
-}
+            public bool OnVieressä(Koordinaatti toinen)
+            {
+                int xEro = Math.Abs(X - toinen.X);
+                int yEro = Math.Abs(Y - toinen.Y);
+                return xEro <= 1 && yEro <= 1 && !(xEro == 0 && yEro == 0);
+            }
+        }
 
-class Program
-{
-    static void Main()
-    {
-        Koordinaatti k1 = new Koordinaatti(2, 3);
-        Koordinaatti k2 = new Koordinaatti(3, 3);
-        Koordinaatti k3 = new Koordinaatti(2, 4);
-        Koordinaatti k4 = new Koordinaatti(5, 5);
+        static void Main(string[] args)
+        {
+            Koordinaatti kohde = new Koordinaatti(0, 0);
 
-        Console.WriteLine($"{k1} ja {k2} vierekkäin? {k1.OnVierekkaen(k2)}");
-        Console.WriteLine($"{k1} ja {k3} vierekkäin? {k1.OnVierekkaen(k3)}");
-        Console.WriteLine($"{k1} ja {k4} vierekkäin? {k1.OnVierekkaen(k4)}");
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    Koordinaatti testi = new Koordinaatti(x, y);
+                    if (testi.OnVieressä(kohde))
+                        Console.WriteLine("Annettu koordinaatti " + x + "," + y + " on koordinaatin 0,0 vieressä.");
+                    else
+                        Console.WriteLine("Annettu koordinaatti " + x + "," + y + " on koordinaatissa 0,0.");
+                }
+            }
+        }
     }
 }
