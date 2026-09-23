962768dc3146459c713e75e88d23462b66039efb
Mustafaa4 <Mustafa.Abdi@edu.bc.fi>
2026-09-23 04:54:27 +0300

    Update väritetyttavarat7.cs

--- väritetyttavarat7.cs
+++ väritetyttavarat7.cs
@@ -1,62 +1,56 @@
-﻿using System;
-
-public class Tavara
+﻿namespace VäritetytTavarat
 {
-    public double Paino { get; }
-    public double Tilavuus { get; }
-
-    public Tavara(double paino, double tilavuus)
+    internal class Program
     {
-        Paino = paino;
-        Tilavuus = tilavuus;
-    }
-
-    public override string ToString()
-    {
-        return "Tavara";
-    }
-}
-
-public class Miekka : Tavara
-{
-    public Miekka() : base(5.0, 3.0) { }
-    public override string ToString() => "Miekka";
-}
-
-public class Kirves : Tavara
-{
-    public Kirves() : base(6.0, 3.5) { }
-    public override string ToString() => "Kirves";
-}
-
-public class VaritettyTavara<T>
-{
-    public T Tavara { get; }
-    public ConsoleColor Vari { get; }
-
-    public VaritettyTavara(T tavara, ConsoleColor vari)
-    {
-        Tavara = tavara;
-        Vari = vari;
-    }
-
-    public void NaytaTavara()
-    {
-        var alkuperainenVari = Console.ForegroundColor;
-        Console.ForegroundColor = Vari;
-        Console.WriteLine(Tavara.ToString());
-        Console.ForegroundColor = alkuperainenVari;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        var punainenMiekka = new VaritettyTavara<Miekka>(new Miekka(), ConsoleColor.Red);
-        var sininenKirves = new VaritettyTavara<Kirves>(new Kirves(), ConsoleColor.Blue);
-
-        punainenMiekka.NaytaTavara();
-        sininenKirves.NaytaTavara();
+        class Tavara
+        {
+            public double Paino { get; }
+            public double Tilavuus { get; }
+            public Tavara(double paino, double tilavuus)
+            {
+                Paino = paino;
+                Tilavuus = tilavuus;
+            }
+        }
+        class Miekka : Tavara
+        {
+            public Miekka() : base(5, 3) { }
+            public override string ToString() { return "Miekka"; }
+        }
+        class Jousi : Tavara
+        {
+            public Jousi() : base(1, 4) { }
+            public override string ToString() { return "Jousi"; }
+        }
+        class Kirves : Tavara
+        {
+            public Kirves() : base(3, 2) { }
+            public override string ToString() { return "Kirves"; }
+        }
+        class VäritettyTavara<T>
+        {
+            public T Tavara { get; }
+            public ConsoleColor Väri { get; }
+            public VäritettyTavara(T tavara, ConsoleColor väri)
+            {
+                Tavara = tavara;
+                Väri = väri;
+            }
+            public void NäytäTavara()
+            {
+                Console.ForegroundColor = Väri;
+                Console.WriteLine(Tavara.ToString());
+                Console.ResetColor();
+            }
+        }
+        static void Main(string[] args)
+        {
+            VäritettyTavara<Miekka> väritettyMiekka = new VäritettyTavara<Miekka>(new Miekka(), ConsoleColor.Red);
+            VäritettyTavara<Jousi> väritettyJousi = new VäritettyTavara<Jousi>(new Jousi(), ConsoleColor.Green);
+            VäritettyTavara<Kirves> väritettyKirves = new VäritettyTavara<Kirves>(new Kirves(), ConsoleColor.Yellow);
+            väritettyMiekka.NäytäTavara();
+            väritettyJousi.NäytäTavara();
+            väritettyKirves.NäytäTavara();
+        }
     }
 }
